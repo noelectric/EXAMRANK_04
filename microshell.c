2b54547660c0b361bbba3ce810611e781362a317
Younes Imazouaren <yimazoua@e1r1p4.1337.ma>
2023-04-17 07:27:28 +0000

    how

--- microshell.c
+++ microshell.c
@@ -1,78 +1,73 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
+#include <libc.h>
 
-int i_error(char *str, char *arg)
+int	_error(char *str, char *arg)
 {
-	while (*str)
-		write(2, str++, 1);
+	while(*str)
+		write (2, str++, 1);
 	if (arg)
 		while (*arg)
-		 write(2, arg++, 1);
-	write(2, "\n", 1);
-	return 1;
+			write(2, arg++, 1);
+	write (2, "\n", 1);
+	return (1);
 }
 
-int i_exec(char **av, int tmp, char **env)
+int	ft_execve(char **av, int i, int tmp, char **env)
 {
-	av[0] = NULL;
+	av[i] = NULL;
 	dup2(tmp, 0);
 	close(tmp);
 	execve(av[0], av, env);
-	return(i_error("error : cannot excute", av[0]));
+	return (_error("error: cannot execute "), av[0]);
 }
 
 int main(int ac, char **av, char **env)
 {
 	int i = 0;
-	int tmp_fd;
+	int tmp_fd = dup(0);
 	int fd[2];
 	(void)ac;
 
-	while(av[i] && av[i + 1])
+	while (av[i] && av[i + 1])
 	{
 		av = &av[i + 1];
 		i = 0;
 		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
 			i++;
-		if(!strcmp(av[0], "cd"))
+		if (strcmp(av[0], "cd") == 0)
 		{
 			if (i != 2)
-				i_error("error: cd: bad arguments", NULL);
-			else if (chdir(av[1]) != 0)
-				i_error("error: cd: cannot change directory to ", av[1]);
-		}
-		else if (i != 0 && (av[i] != 0 || strcmp(av[i], ";") == 0))
+				_error("error: cd: bad arguments ", NULL);
+			else if (chdir(var[1]) != 0)
+				_error("error: cd: cannot change directory to ", av[1]);
+		}	
+		else if (i != 0 && (av[i] == NULL || strcmp(av[i], ";") == 0))
 		{
 			if (fork() == 0)
-			{
-				if (i_exec(av, tmp_fd, env))
+				if (ft_execve(av, i, tmp_fd, env))
 					return 1;
-			}
 			else
 			{
 				close(tmp_fd);
-				while (waitpid(-1, NULL, 0))
+				while (waitpid(-1, NULL, 0) != -1)
 					;
 				tmp_fd = dup(0);
 			}
 		}
-		else if(i != 0 && strcmp(av[i], "|"))
+		else if (i != 0 && strcmp(av[i], "|") == 0)
 		{
 			pipe(fd);
-			if(fork() == 0)
+			if (fork() == 0)
 			{
 				dup2(fd[1], 1);
+				close(fd[0]);
 				close(fd[1]);
-				close(tmp_fd);
-				if (i_exec(av, tmp_fd, env))
-					return 1;				
+				if (ft_execve(av, i, tmp_fd, env))
+					return 1;
 			}
 			else
 			{
 				close(fd[1]);
-				close (tmp_fd);
+				close(tmp_fd);
 				tmp_fd = fd[0];
 			}
 		}
